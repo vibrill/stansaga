0cfae8f044d89f04ddfe98abd7e214ec68293857
vbrillianto <vbrillianto@gmail.com>
2022-01-08 10:20:30 +0800

    menambahkan kode rek

--- stanSaga.go
+++ stanSaga.go
@@ -24,27 +24,49 @@ func CreateDB(namadb string) {
 	file.Close()
 }
 
-func getfileXLSX() (listnama []string) {
+func getfileXLSX() (listnama []string, detektor []string) {
+	var listxls []string
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, file := range files {
 		fn := file.Name()
-		fn = fn[len(fn)-5:]
-
-		if fn == ".xlsx" {
-			listnama = append(listnama, file.Name())
+		if len(fn) > 5 {
+			x := len(fn) - 5
+			if fn[x:] == ".xlsx" {
+				listxls = append(listxls, file.Name())
+			}
 		}
 	}
-	return listnama
+	for _, file := range listxls {
+		f, err := excelize.OpenFile(file)
+		if err == nil {
+			deteksi := "MODAL.db"
+			for i := 0; i < len(f.GetSheetList()); i++ {
+				namaSheet := f.GetSheetName(i)
+				if namaSheet == "SSH1" {
+					deteksi = "BARJAS.db"
+					break
+				}
+				if namaSheet == "KODE BELANJA" {
+					deteksi = "KODEREK.db"
+					break
+				}
+			}
+			listnama = append(listnama, file)
+			detektor = append(detektor, deteksi)
+		}
+	}
+	return listnama, detektor
 }
 
 func main() {
-	pathlist := getfileXLSX()
-	for _, path := range pathlist {
+	pathlist, detektor := getfileXLSX()
+	for i := 0; i < len(pathlist); i++ {
+		path := pathlist[i]
+		namadb := detektor[i]
 		//membuat database
-		namadb := path[:len(path)-5] + ".db"
 		fmt.Println("membuat database", namadb)
 		CreateDB(namadb)
 
@@ -79,18 +101,24 @@ func main() {
 
 		f, _ = excelize.OpenFile(path)
 		banyakSheet = f.SheetCount
+		//println("ini adalah banyak sheet:", banyakSheet)
 		for i := 0; i < banyakSheet; i++ {
 			sheetName := f.GetSheetName(i)
+			//println("ini adalah nama sheet: ", sheetName)
 			rows, err := f.Rows(sheetName)
 			allrow, _ := f.GetRows(sheetName)
 			totalbaris := len(allrow)
+			//print("ini total baris :", totalbaris, "\n")
 			if err != nil {
 				fmt.Println(err, "copy kelima")
 			}
 
 			//membuat tabel database
 			db, _ := sql.Open("sqlite3", "./"+namadb) // Open the created SQLite File
-			execs := "CREATE TABLE " + sheetName + " (NO TEXT, KODE TEXT, URAIAN TEXT, SPESIFIKASI TEXT, SATUAN TEXT, HARGA_SATUAN TEXT, KETERANGAN TEXT, SUBJUDUL TEXT)"
+			execs := "CREATE TABLE " + strings.ReplaceAll(sheetName, " ", "_") + " (NO TEXT, KODE TEXT, URAIAN TEXT, SPESIFIKASI TEXT, SATUAN TEXT, HARGA_SATUAN TEXT, KETERANGAN TEXT, SUBJUDUL TEXT)"
+			if namadb == "KODEREK.db" {
+				execs = "CREATE TABLE " + strings.ReplaceAll(sheetName, " ", "_") + " (NO TEXT, JENIS_BELANJA TEXT, KODE TEXT, URAIAN TEXT, KETERANGAN TEXT)"
+			}
 			statement, err := db.Prepare(execs)
 			if err != nil {
 				fmt.Println("keempat" + err.Error())
@@ -99,10 +127,6 @@ func main() {
 
 			//next untuk melewati judul
 			rows.Next()
-			_, err = rows.Columns()
-			if err != nil {
-				fmt.Println(err, "keenam")
-			}
 
 			//mendapatkan data perbaris meletakkannnya pada list data
 			fmt.Println("Generate Table", sheetName, ", please wait!")
@@ -116,67 +140,117 @@ func main() {
 				if bariskosong == 3 {
 					break
 				}
-				rows.Next()
 				row, err := rows.Columns()
+				//println("ini adalah row : ", row)
 				if err != nil {
 					fmt.Println(err, "ketujuh")
 				}
-				x := 0
+
 				//memproses data perkolom pada setiap baris
+
 				var data [7]string
-				for _, colCell := range row {
-					data[x] = strings.ReplaceAll(colCell, `"`, "")
-					x++
-				}
-
-				//deteksi kolom kosong
-				kolomkosong := 0
-				for _, item := range data {
-					if item == "" {
-						kolomkosong += 1
-					}
-				}
-
-				//jika kolom kosong == 7 baris kosong +1
-				if kolomkosong == 7 {
-					bariskosong += 1
-				}
-				if kolomkosong != 7 {
-					bariskosong = 0
-				}
-
-				//mendapatkan value cell per baris
+				var datakorek [4]string
 				text := ""
-				for _, item := range data { //sepadan dengan "for item in data:" pada python
-					text = text + `"` + item + `",`
-				}
-				text = text[:len(text)-1] //omit last coma
+				x := 0
+				if namadb == "KODEREK.db" {
+					rows.Next()
+					for _, colCell := range row {
+						//fmt.Println("ini adalah collcel: ", colCell)
+						datakorek[x] = strings.ReplaceAll(colCell, `"`, "")
+						x++
+					}
+					//deteksi kolom kosong
+					kolomkosong := 0
+					for _, item := range datakorek {
+						if item == "" {
+							kolomkosong += 1
+						}
+					}
 
-				//mengubah indikatorsubjudul
-				if kolomkosong == 6 {
-					subjudul = data[0]
-				}
+					//jika kolom kosong == 4 baris kosong +1
+					if kolomkosong == 4 {
+						bariskosong += 1
+					}
+					if kolomkosong != 4 {
+						bariskosong = 0
+					}
+					//fmt.Println("ini adalah datakorek : ", datakorek)
+					for _, item := range datakorek {
+						text = text + `"` + item + `",`
 
-				//memasukkan keterangan subjudul jika bukan subjudul dan baris kosong
-				if kolomkosong != 6 && kolomkosong != 7 {
-					text = text + `"` + subjudul + `"`
-				} else {
-					text = text + `"empty"`
-				}
+					}
 
-				//insert data
-				if kolomkosong != 7 {
+					//fmt.Println("ini teks yang akan ditulis: ", text)
+					text = text[:len(text)-1]
 					noIncrement += 1
-					execs := "INSERT INTO " + sheetName + ` VALUES ("` + strconv.Itoa(noIncrement) + `",` + text + ")"
-					statement, err := db.Prepare(execs) // Prepare statement.
-					if err != nil {
-						fmt.Println(err.Error())
+					if bariskosong != 1 {
+						execs := "INSERT INTO " + strings.ReplaceAll(sheetName, " ", "_") + ` VALUES ("` + strconv.Itoa(noIncrement) + `",` + text + ")"
+						statement, err := db.Prepare(execs) // Prepare statement.
+						if err != nil {
+							fmt.Println(err.Error())
+						}
+						_, err = statement.Exec()
+						if err != nil {
+							fmt.Println(err.Error())
+						}
 					}
-					_, err = statement.Exec()
-					if err != nil {
-						fmt.Println(err.Error())
+
+				} else {
+					rows.Next()
+					for _, colCell := range row {
+						data[x] = strings.ReplaceAll(colCell, `"`, "")
+						x++
+					}
+
+					//deteksi kolom kosong
+					kolomkosong := 0
+					for _, item := range data {
+						if item == "" {
+							kolomkosong += 1
+						}
+					}
+
+					//jika kolom kosong == 7 baris kosong +1
+					if kolomkosong == 7 {
+						bariskosong += 1
+					}
+					if kolomkosong != 7 {
+						bariskosong = 0
+					}
+
+					//mendapatkan value cell per baris
+					for _, item := range data { //sepadan dengan "for item in data:" pada python
+						text = text + `"` + item + `",`
+					}
+					text = text[:len(text)-1] //omit last coma
+
+					//mengubah indikatorsubjudul
+					if kolomkosong == 6 {
+						subjudul = data[0]
+					}
+
+					//memasukkan keterangan subjudul jika bukan subjudul dan baris kosong
+					if kolomkosong != 6 && kolomkosong != 7 {
+						text = text + `"` + subjudul + `"`
+					} else {
+						text = text + `"empty"`
+					}
+
+					//insert data
+					if kolomkosong != 7 {
+						noIncrement += 1
+						execs := "INSERT INTO " + strings.ReplaceAll(sheetName, " ", "_") + ` VALUES ("` + strconv.Itoa(noIncrement) + `",` + text + ")"
+						statement, err := db.Prepare(execs) // Prepare statement.
+						if err != nil {
+							fmt.Println(err.Error())
+						}
+						_, err = statement.Exec()
+						if err != nil {
+							fmt.Println(err.Error())
+						}
 					}
 				}
+
 			}
 			db.Close()
 
